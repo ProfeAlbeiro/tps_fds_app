8de828f7cc40fde190474649380864fc349f20a2
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-11-06 15:42:13 -0500

    3. Prototipo NO Funcional

--- docs/trim1/8_prototipo_no_funcional/4_prototipo_no_funcional/js/scripts.js
+++ docs/trim1/8_prototipo_no_funcional/4_prototipo_no_funcional/js/scripts.js
@@ -21,21 +21,36 @@ function validarLogin() {
     // Frena el evento: Click
     event.preventDefault();
     // Captura el valor que tienen los controles del formulario
+    let correoPatron = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
     let correo = document.getElementById('correo').value;
-    let pass = document.getElementById('pass').value;
-    // Validar el acceso al Dashboard: Administrador
-    if (correo == "admin@correo.com" && pass == "12345") {
-        window.location = '../admin.html';
-    } 
-    // Validar el acceso al Dashboard: Cliente
-    else if (correo == "cliente@correo.com" && pass == "12345") {
-        window.location = '../customer.html';
+    let pass = document.getElementById('pass').value; 
+    // Correo: Cuando está vacío
+    if (correo === "") {
+        alert("El Correo NO puede estar vacío");
+        document.getElementById('correo').focus();
     }
-    // Validar el acceso al Dashboard: Vendedor
-    else if (correo == "vendedor@correo.com" && pass == "12345") {
-        window.location = '../seller.html';
+    // Correo: Es un correo (@, .com, .es, etc)
+    else if (!correoPatron.test(correo)) {
+        alert("No es un correo válido");
+        document.getElementById('correo').focus();
     }
     else {
-        alert('Los datos son incorrectos');
-    }    
+        // Validar el acceso al Dashboard: Administrador
+        if (correo == "admin@correo.com" && pass == "12345") {
+            window.location = '../admin.html';
+        }
+        // Validar el acceso al Dashboard: Cliente
+        else if (correo == "cliente@correo.com" && pass == "12345") {
+            window.location = '../customer.html';
+        }
+        // Validar el acceso al Dashboard: Vendedor
+        else if (correo == "vendedor@correo.com" && pass == "12345") {
+            window.location = '../seller.html';
+        }
+        // Datos Incorrectos
+        else {
+            alert('Los datos son incorrectos');
+        }  
+    }
+      
 }
