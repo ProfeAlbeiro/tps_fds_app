60566bfe2356701602a56c569f9ea42b9cbce9f0
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-11-06 11:52:52 -0500

    3. Prototipo NO Funcional

--- docs/trim1/8_prototipo_no_funcional/4_prototipo_no_funcional/js/scripts.js
+++ docs/trim1/8_prototipo_no_funcional/4_prototipo_no_funcional/js/scripts.js
@@ -1,1 +1,7 @@
-// alert('Hola Mundo!!!');
+function validarLogin() {
+    event.preventDefault();
+    let correo = document.getElementById('correo').value;
+    let pass = document.getElementById('pass').value;
+    console.log("Su correo modificado es: " + correo + " y su password es: " + pass);
+    // alert("Su correo es: " + correo + " y su password es: " + pass);
+}
