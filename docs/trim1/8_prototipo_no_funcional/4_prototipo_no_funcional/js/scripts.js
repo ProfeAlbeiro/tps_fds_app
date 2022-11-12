e2dfd6b718628b1e11cf77beab641ee9355acf26
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-11-12 12:19:12 -0500

    Prototipo No Funcional

--- docs/trim1/8_prototipo_no_funcional/4_prototipo_no_funcional/js/scripts.js
+++ docs/trim1/8_prototipo_no_funcional/4_prototipo_no_funcional/js/scripts.js
@@ -11,19 +11,59 @@ function mainIndex() {
     id = event.target.getAttribute("id");
     if (id == "submint_login") {
         validarLogin();
+    } else if (id == "submit-contac") {
+        validarContac();
     }
 }
 
 /* -------------------------------------------------------------------------------- */
 /* FUNCIONES ---------------------------------------------------------------------- */
 /* -------------------------------------------------------------------------------- */
-function validarLogin() {
+// Validar Contacto
+function validarContac() {
+    // Captura el valor que tienen los controles del formulario
+    nombres = document.getElementById('nombres').value;
+    apellidos = document.getElementById('apellidos').value;
+    correo = document.getElementById('correo-cont').value;
+    asunto = document.getElementById('asunto').value;
+    mensaje = document.getElementById('mensaje').value;
+    // Expresiones Regulares para validar controles del formulario
+    let textoPatron = /^[ a-zA-ZáéíóúÁÉÍÓÚäëïöüÄËÏÖÜàèìòùÀÈÌÒÙñÑ]+$/;
+    let correoPatron = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
     // Frena el evento: Click
     event.preventDefault();
+    // Nombres: Cuando está vacío
+    if (nombres === "") {
+        alert("Los Nombres NO pueden estar vacíos");
+        document.getElementById('nombres').focus();
+    }
+    // Nombres: No puede contener números o caracteres especiales
+    else if (!textoPatron.test(nombres)) {
+        alert("Los Nombres NO pueden contener números o caracteres especiales");
+        document.getElementById('nombres').focus();
+    }
+    // Nombres: No puede contener números o caracteres especiales
+    else if (nombres.length < 2 || nombres.length > 50) {
+        alert("Los Nombres deben tener entre 2 y 50 caracteres");
+        document.getElementById('nombres').focus();
+    }
+    // Se envía el mensaje
+    else {
+        alert("El mensaje se ha enviado correctamente. En menos de 24 horas el Administrador se comunicará con Usted por medio de su Correo Electrónico");
+        window.location = 'index.html#contactenos';        
+    }
+}
+
+// Validar Login
+function validarLogin() {
     // Captura el valor que tienen los controles del formulario
-    let correoPatron = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
     let correo = document.getElementById('correo').value;
     let pass = document.getElementById('pass').value; 
+    // Expresiones Regulares para validar controles del formulario
+    let TextoPatron = /^[ a-zA-ZáéíóúÁÉÍÓÚäëïöüÄËÏÖÜàèìòùÀÈÌÒÙñÑ]+$/;
+    let correoPatron = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    // Frena el evento: Click
+    event.preventDefault();
     // Correo: Cuando está vacío
     if (correo === "") {
         alert("El Correo NO puede estar vacío");
@@ -46,15 +86,15 @@ function validarLogin() {
     }
     else {
         // Validar el acceso al Dashboard: Administrador
-        if (correo == "admin@correo.com" && pass == "12345") {
+        if (correo === "admin@correo.com" && pass == "12345") {
             window.location = '../admin.html';
         }
         // Validar el acceso al Dashboard: Cliente
-        else if (correo == "cliente@correo.com" && pass == "12345") {
+        else if (correo === "cliente@correo.com" && pass == "12345") {
             window.location = '../customer.html';
         }
         // Validar el acceso al Dashboard: Vendedor
-        else if (correo == "vendedor@correo.com" && pass == "12345") {
+        else if (correo === "vendedor@correo.com" && pass == "12345") {
             window.location = '../seller.html';
         }
         // Datos Incorrectos
